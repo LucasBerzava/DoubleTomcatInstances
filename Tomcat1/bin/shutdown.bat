6e4cf1695eed45fbcd67a161c5a8dd066aba6940
Lucas Berzava <Lucas Berzava@DESKTOP-JG0OTO3>
2020-04-30 20:15:58 +0300

    commit

---
+++ Tomcat1/bin/shutdown.bat
@@ -0,0 +1,3 @@
+set CATALINA_HOME=C:\Program Files\Apache Software Foundation\Tomcat 9.0
+set CATALINA_BASE=C:\Users\Lucas Berzava\git\crunchify\Tomcat1
+C:\Program Files\Apache Software Foundation\Tomcat 9.0\bin\shutdown.bat
---
+++ Tomcat2/bin/startup.bat
@@ -0,0 +1,3 @@
+set CATALINA_HOMEC:\Program Files\Apache Software Foundation\Tomcat 9.0
+set CATALINA_BASE=C:\Users\Lucas Berzava\git\crunchify\Tomcat2
+C:\Program Files\Apache Software Foundation\Tomcat 9.0\bin\startup.bat
